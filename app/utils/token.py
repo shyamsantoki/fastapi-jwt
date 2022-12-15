2cd670ea3b2032824335c89829ef6484c051beff
Shyam Santoki <shyam_santoki@outlook.com>
2022-12-15 12:57:13 +0530

    removed unnecessary code

--- app/utils/token.py
+++ app/utils/token.py
@@ -4,7 +4,6 @@ from typing import Any, Union
 
 from dotenv import load_dotenv
 from jose import jwt
-from passlib.context import CryptContext
 
 load_dotenv()
 
@@ -13,16 +12,6 @@ REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 ALGORITHM = "HS256"
 JWT_SECRET_KEY = getenv("JWT_SECRET_KEY")  # should be kept secret
 JWT_REFRESH_SECRET_KEY = getenv("JWT_REFRESH_SECRET_KEY")  # should be kept secret
-
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def get_hashed_password(password: str) -> str:
-    return password_context.hash(password)
-
-
-def verify_password(password: str, hashed_pass: str) -> bool:
-    return password_context.verify(password, hashed_pass)
 
 
 def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
@@ -47,6 +36,3 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) ->
     to_encode = {"exp": expires_delta, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
     return encoded_jwt
-
-
-get_hashed_password("shyam")
