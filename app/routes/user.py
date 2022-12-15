f6ef398dc7941238f50f3a9cd29816903e66776a
Shyam Santoki <shyam_santoki@outlook.com>
2022-12-15 12:58:18 +0530

    added routes for signup and login endpoints

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,18 +1,31 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.models.user import User
-from app.schemas.user import CreateUser
-from app.utils.schema import get_db
+from app.schemas.user import DisplayUser, LoginUser, SignupUser
+from app.utils.password import get_hashed_password, verify_password
+from app.utils.schema import HTTPError, get_db
+from app.utils.token import create_access_token, create_refresh_token
 
 router = APIRouter()
 
 
-@router.post("/user")
-def create_user(request: CreateUser, db: Session = Depends(get_db)):
+@router.post(
+    "/user/signup",
+    description="Signing up a new user",
+    responses={
+        status.HTTP_201_CREATED: {"model": DisplayUser},
+        status.HTTP_409_CONFLICT: {"model": HTTPError},
+    },
+    status_code=status.HTTP_201_CREATED,
+)
+def signup_user(request: SignupUser, db: Session = Depends(get_db)):
+    if db.query(User.username).filter(User.username == request.username).count():
+        raise HTTPException(status.HTTP_409_CONFLICT, detail="Username is taken")
+    print(request.password)
     user = User(
         username=request.username,
-        password=request.password,
+        password=get_hashed_password(request.password),
         first_name=request.first_name,
         last_name=request.last_name,
         email=request.email,
@@ -24,9 +37,34 @@ def create_user(request: CreateUser, db: Session = Depends(get_db)):
     return user
 
 
-# def create_user(request:schemas.user.CreateUser, db: Session = Depends(get_db):
-#     user = models.user.User(
-#         username=request.username,
-#         password=request.password
-
-#     )
+@router.post(
+    "/user/login",
+    description="Logging in an existing user",
+    responses={
+        status.HTTP_200_OK: {"model": DisplayUser},
+        status.HTTP_400_BAD_REQUEST: {"model": HTTPError},
+    },
+    status_code=status.HTTP_200_OK,
+)
+def login_user(request: LoginUser, db: Session = Depends(get_db)):
+    if not db.query(User.id).filter(User.username == request.username).count():
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="Username does not exist"
+        )
+    else:
+        user_creds = (
+            db.query(User.password, User.id)
+            .filter(User.username == request.username)
+            .first()
+        )
+        user_id = user_creds.id
+        user_password = user_creds.password
+        if verify_password(request.password, user_password):
+            return {
+                "access_token": create_access_token(user_id),
+                "refresh_token": create_refresh_token(user_id),
+            }
+        else:
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST, detail="Password is incorrect"
+            )
