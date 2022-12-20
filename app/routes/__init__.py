6a40a599b0bfc9c115b33151650bb0caf70064e6
Shyam Santoki <shyam_santoki@outlook.com>
2022-12-20 12:46:34 +0530

    fixed app

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
-from app.routes import blog, user
+from app.routes import user
 
 app = FastAPI()
 
 app.include_router(user.router)
-app.include_router(blog.router)
 
 
 @app.get("/")
